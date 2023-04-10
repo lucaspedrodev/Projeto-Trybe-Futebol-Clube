7c46b7e9600418ff5348e89bd352d8b0ab69035e
Lucas Pedro <rodrigerioo@gmail.com>
2023-04-10 16:19:09 -0300

    requisito 16

--- app/backend/src/Controllers/matchesController.ts
+++ app/backend/src/Controllers/matchesController.ts
@@ -4,8 +4,18 @@ import MatchesService from '../Services/matchesService';
 export default class MatcherController {
   private _service = new MatchesService();
 
-  public getAllMatches = async (_req: Request, res: Response) => {
+  public getAllMatches = async (req: Request, res: Response) => {
+    const { inProgress } = req.query;
+
     const result = await this._service.getAllMatches();
-    return res.status(200).json(result);
+    let resultOption = result;
+    if (inProgress === 'true') {
+      resultOption = result.filter((match) => match.inProgress === true);
+    }
+    if (inProgress === 'false') {
+      resultOption = result.filter((match) => match.inProgress === false);
+    }
+
+    res.status(200).json(resultOption);
   };
 }
