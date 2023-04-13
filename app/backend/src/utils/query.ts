b80f9c3b647bdfb8694c7dea075f5e27fe9e2e77
Lucas Pedro <rodrigerioo@gmail.com>
2023-04-13 10:40:51 -0300

    requisito 24

--- app/backend/src/utils/query.ts
+++ app/backend/src/utils/query.ts
@@ -12,7 +12,17 @@ SUM(CASE WHEN home_team_goals > away_team_goals THEN 1 ELSE 0 END) AS totalVicto
 SUM(CASE WHEN home_team_goals = away_team_goals THEN 1 ELSE 0 END) AS totalDraws,
 SUM(CASE WHEN home_team_goals < away_team_goals THEN 1 ELSE 0 END) AS totalLosses,
 SUM(home_team_goals) AS goalsFavor, 
-SUM(away_team_goals) AS goalsOwn
+SUM(away_team_goals) AS goalsOwn,
+SUM(home_team_goals - away_team_goals) AS goalsBalance,
+ROUND(
+  SUM(
+    CASE 
+      WHEN home_team_goals > away_team_goals THEN 3 
+      WHEN home_team_goals = away_team_goals THEN 1 
+      ELSE 0 
+    END
+  ) / (COUNT(team_name) * 3) * 100, 
+2) AS efficiency
 FROM 
 TRYBE_FUTEBOL_CLUBE.matches AS matches 
 INNER JOIN TRYBE_FUTEBOL_CLUBE.teams AS teams ON matches.home_team_id = teams.id
@@ -20,5 +30,10 @@ WHERE
 in_progress = false
 GROUP BY 
 name
+ORDER BY 
+totalPoints DESC, 
+totalVictories DESC, 
+goalsBalance DESC, 
+goalsFavor DESC
 `;
 export default query;
